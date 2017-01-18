374d32331619aec90bcf48b436356c153d060663
Wesley Handy <wesleylhandy@gmail.com>
2017-01-17 19:25:13 -0500

    upload fitbit images

---
+++ assets/javascript/logic.js
@@ -0,0 +1,26 @@
+//initialize date
+var d = new Date();
+var year = d.getFullYear();
+
+
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA86KcIckWm_hvCyb8iIqRDB0tkL9q_aZI",
+    authDomain: "marathontrainer-c4507.firebaseapp.com",
+    databaseURL: "https://marathontrainer-c4507.firebaseio.com",
+    storageBucket: "marathontrainer-c4507.appspot.com",
+    messagingSenderId: "89102549201"
+  };
+
+  firebase.initializeApp(config);
+
+//   •	marathon training
+// o	Group
+// o	Create training routes
+// o	Maps api
+// o	Meetup api
+// o	Weather api
+// o	http://www.icreon.us/nyrr.html
+// o	Countdown to marathon
+// o	Training guide
