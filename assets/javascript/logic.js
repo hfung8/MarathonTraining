274aa95a619dba271ea252cfb54d2cd67a1208c5
Franco Sevillano <fasevillano1@gmail.com>
2017-01-21 16:09:07 -0500

    Updates

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,8 +4,19 @@ $(document).ready(function(){
   $('#textarea1').trigger('autoresize');
   $(".button-collapse").sideNav();
 
-    $('.scrollspy').scrollSpy();
 
+// $(window).scroll(function() {
+//     if ($(this).scrollTop() >= 80) {        // If page is scrolled more than 50px
+//         $('#return2').fadeIn(200);    // Fade in the arrow
+//     } else {
+//         $('#return2').fadeOut(200);   // Else fade out the arrow
+//     }
+// });
+
+$('#return2').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 800);
+});
         
- 
 });
