ae403b58bb5e5402baa3ab7f13032a77c5e9e954
Wesley Handy <wesleylhandy@gmail.com>
2017-01-24 14:09:13 -0500

    logo path corrected

--- assets/javascript/login-handlers.js
+++ assets/javascript/login-handlers.js
@@ -153,17 +153,13 @@ function initApp() {
 
   });
   // [END authstatelistener]
-  $(".login-btn").click(function(){
-    $(".login").show('fast');
-    $(".register").hide('fast');
-  });
-  $(".register-btn").click(function(){
-    $('.register').show('fast');
-    $(".login").hide('fast');
-  });
+
+
   $('#email-login').click(toggleSignIn);
   $('#sign-up').click(handleSignUp);
   $('#sign-out').click(toggleSignIn);
+
+
   $("#fitbit-login").click(function(){
     var win = window.open("https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=2283WS&redirect_uri=http%3A%2F%2F127.0.0.1%3A8080%2Findex.html&scope=activity%20heartrate%20location%20profile%20weight&expires_in=604800", '_blank');
       win.focus();
@@ -174,5 +170,23 @@ function initApp() {
 }
 
 $(document).ready(function() {
-  initApp();
+  initApp();  
+
+  $(".login-btn").click(function(){
+    $(".login").show('fast');
+    $(".register").hide('fast');
+  });
+  
+  $(".register-btn").click(function(){
+    $('.register').show('fast');
+    $(".login").hide('fast');
+  });
+    $(".login-btn").click(function(){
+    $(".login").show('fast');
+    $(".register").hide('fast');
+  });
+  $(".register-btn").click(function(){
+    $('.register').show('fast');
+    $(".login").hide('fast');
+  });
 });
